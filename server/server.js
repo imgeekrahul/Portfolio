f2f4247cf95ee911d1f07b2f0778041bb97d6e9f
Rah <hackrahul124@gmail.com>
2023-11-26 17:02:45 +0530

    added /

--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const route = express.Router();
 
+route.get("/", (req, res)=> {
+    res.status(200).json("App is running");
+})
+
 route.post("/add_request", async (req, res) => {
     try {
         res.json("Hii");
