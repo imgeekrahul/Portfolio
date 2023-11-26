f3d6701af001212c569e0de6226c8c50d2a442d7
Rah <hackrahul124@gmail.com>
2023-11-26 17:04:12 +0530

    fix: minor API fixes

--- server/server.js
+++ server/server.js
@@ -8,21 +8,20 @@ route.get("/", (req, res)=> {
 
 route.post("/add_request", async (req, res) => {
     try {
-        res.json("Hii");
-        // const name = req.body.name;
-        // const email = req.body.email;
-        // const subject = req.body.subject;
-        // const message = req.body.message;
+        const name = req.body.name;
+        const email = req.body.email;
+        const subject = req.body.subject;
+        const message = req.body.message;
     
-        // const data = {
-        //     name: name,
-        //     email: email,
-        //     subject: subject,
-        //     message: message
-        // }
+        const data = {
+            name: name,
+            email: email,
+            subject: subject,
+            message: message
+        }
 
-        // await portFolio.create(data);
-        // res.status(201).json(data);
+        await portFolio.create(data);
+        res.status(201).json(data);
     } catch(err) {
         console.log(err);
     }
